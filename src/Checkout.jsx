a659a87f2f13ff0c992e47c0f726c6fa7a958d92
Lewis <lewis.burgess314@gmail.com>
2025-03-06 10:19:50 +0000

    Temp integrated paypal frontend

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,7 +1,22 @@
 import { PayPalButtons } from "@paypal/react-paypal-js"
 import paypalService from './services/paypalService'
-const Checkout = () => {
-  return (<PayPalButtons createOrder={paypalService.createOrder} onApprove={paypalService.onApprove}/>)
+import { useDispatch } from "react-redux"
+import { notify } from './reducers/notificationReducer'
+const Checkout = ({ cart }) => {
+  const dispatch = useDispatch()
+
+  const dispatchNotify = (obj) => {
+    dispatch(notify(obj))
+  }
+  return (<PayPalButtons 
+    createOrder={() => {paypalService.createOrder(cart, dispatchNotify)}} 
+    onApprove={(data, actions) => {paypalService.onApprove(data, actions, dispatchNotify)}}
+    style={{
+      shape: "pill",
+      layout: "vertical",
+      color: "blue",
+      label: "pay",
+  }}/>)
 }
 
 export default Checkout
--- src/services/paypalService.js
+++ src/services/paypalService.js
@@ -1,11 +1,63 @@
+import axios from "axios"
 import { PAYPALCLIENTID } from "./utils"
 
-const createOrder = async () => {
-  // TODO
+// This callback is called by the SDK when the user presses a payment option
+// It is requesting an order id from the merchant server so that it can instantiate a popup to confirm payment
+// Takes a cart object containing an array of product ids and quantities
+const createOrder = async (cart, notify) => {
+  try {
+    // Attempts to create an order on the backend
+    const { data } = await axios.post('/api/orders', {cart})
+    if (data.id){
+      return data.id
+    } else {
+      const errorDetail = data?.details?.[0]
+      const errorMessage = errorDetail 
+        ? `${errorDetail.issue} ${errorDetail.description} (${data.debug_id})`
+        : JSON.stringify(data)
+      throw new Error(errorMessage)
+    }
+
+  } catch (error){
+    console.error(error)
+    notify({
+      message: error.message,
+      severity: 'error'
+    })
+  }
 }
 
-const onAprove = async () => {
-  // TODO
+// After the user has approved the payment, this callback makes a call to the merchant server to capture the payment
+const onApprove = async (data, actions, notify) => {
+  try {
+    const { orderData } = await axios.post(`/api/orders/capture/${data.orderID}`)
+
+    // Attempts handle error cases, recoverable and non-recoverable
+    const errorDetail = orderData?.details?.[0]
+
+    // Recoverable instrument declined- retry capture
+    if (errorDetail?.issue === 'INTRUMENT_DECLINED'){
+      return actions.restart()
+    // Non-recoverable
+    } else if (errorDetail){
+      throw new Error(`${errorDetail.description} (${orderData.debug_id})`)
+      // Handles SUCCESS
+    } else {
+      const transaction = orderData.purchase_units[0].payments.captures[0]
+      console.log(transaction)
+      notify({
+        message: `Purchase successfull!!:`,
+        severity: 'success'
+      })
+    }
+
+  } catch (error){
+    console.error(error)
+    notify({
+      message: `Sorry, your transaction could not be processed: ${error.message}`,
+      severity: 'error'
+    })
+  }
 }
 
 const paypalInitialOptions = {
@@ -13,4 +65,4 @@ const paypalInitialOptions = {
 
 }
 
-export default {createOrder, onAprove, paypalInitialOptions}
+export default {createOrder, onApprove, paypalInitialOptions}
